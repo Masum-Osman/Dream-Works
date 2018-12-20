5608416966457e485245fe76bb5cbe9ba0262962
Masum-Osman <ogkmasum@gmail.com>
2018-12-20 22:18:37 +0600

    finally Final

--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -11,7 +11,7 @@ void init(void)
 	glClearColor(0.1, 0.1, 0.2, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(0, 1500, 0, 900);
-	PlaySound("James Bond.wav", NULL, SND_ASYNC|SND_FILENAME|SND_LOOP);
+	//PlaySound("James Bond.wav", NULL, SND_ASYNC|SND_FILENAME|SND_LOOP);
 }
 
 void walkingMan(void)
